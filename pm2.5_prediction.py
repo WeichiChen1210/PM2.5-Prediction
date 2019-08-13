348a48b695fbb0526bd10132f04fc90392f28d62
WeichiChen1210 <iphone31302@gmail.com>
2019-08-14 03:00:12 +0800

    Do the simple prediction with wind speed

--- pm2.5_prediction.py
+++ pm2.5_prediction.py
@@ -71,16 +71,24 @@ def get_all_data():
       value['date'] = taiwan_aware
     return data
 
+# Reconstruct time infomation by `month`, `day`, and `hour`
+
+def get_time(x):
+    time_str = '2019 %d %d %d' % (x[0], x[1], x[2])
+    taipei_tz = pytz.timezone('Asia/Taipei')
+    time = dt.datetime.strptime(time_str, '%Y %m %d %H').replace(tzinfo=taipei_tz)
+    return time
 
 #today = int(dt.datetime.now().strftime("%d"))
 
 #%% get data
 pos5 = get_data_by_pos(5)
-
+pos3 = get_data_by_pos(3)
 df5 = pd.DataFrame(pos5)
+df3 = pd.DataFrame(pos3)
 #%%
 # Input time
-time = ['2019 06 01', '2019 08 08']
+time = ['2019 06 01', '2019 08 10']
 taipei_tz = pytz.timezone('Asia/Taipei')
 
 # Set time
@@ -166,7 +174,7 @@ for month in range(6, 9):
     if month == 6:
         max_day = 31
     if month == 8:
-        max_day = 8
+        max_day = 10
     month_str = str(month)
     for day in range(1, max_day):
         # if the day is missing, ignore
@@ -202,21 +210,158 @@ df5mean = pd.read_csv('pos5.csv', index_col=0)
 
 df_wind = pd.read_csv('wind.csv', index_col=0)
 df_wind = df_wind.drop(['month', 'day', 'hour'], axis=1)
-
-#%% concate the two frames
+#%%
+# concate the two frames
 df5mean = pd.concat([df5mean, df_wind], axis=1, sort=False)
 
+#%%
+df5mean['time'] = df5mean[['month', 'day', 'hour']].apply(get_time, axis=1)
+
+#%%
+# Add explicitly converter
+pd.plotting.register_matplotlib_converters()
+# Plt
+plt.figure(figsize=(12, 7))
+plt.scatter(df5mean['time'], df5mean['pm2.5'])
+#%%
+df5mean[['pm2.5_shift-1']] = df5mean[['pm2.5']].shift(-1)
 
 
+#%%
 
 
+df5mean[['time_shift-1']] = df5mean[['time']].shift(-1)
 
 
+#%%
 
 
+df5mean.head()
 
 
+#%%
 
+
+# check the next row is the next hour or not. 
+# If it is not, the `pm2.5_next_hour` column will be given NaN.
+
+def check_next_hour(x):
+    one_hour = dt.timedelta(hours=1)
+    if x[2] - x[1] == one_hour:
+        return x[0]
+    return np.nan
+
+df5mean['pm2.5_next_hour'] = df5mean[['pm2.5_shift-1', 'time', 'time_shift-1']].apply(check_next_hour, axis=1)
+
+
+#%%
+
+
+df5mean.head()
+
+
+#%%
+
+
+df5mean.isna().sum()
+
+
+#%%
+
+
+# Discard rows that contain NaN value
+df5mean.dropna(inplace=True)
+
+
+#%%
+
+
+df5mean.isna().sum()
+
+
+# ### Normalization
+# 
+# $z = \frac{x- \mu}{\sigma}$
+# 
+
+#%%
+
+# Save time infomation in another df, and discard it
+df5mean_time = df5mean['time_shift-1']
+df5mean.drop(columns=['time', 'time_shift-1'], axis=0, inplace=True)
+# Normalization
+df5mean = (df5mean - df5mean.mean()) / df5mean.std()
+
+
+# ### Divid training set and test set
+
+#%%
+
+four_fifth_len = len(df5mean)*0.8
+four_fifth_len = int(four_fifth_len)
+
+
+#%%
+
+train_df = df5mean[:four_fifth_len]
+test_df = df5mean[four_fifth_len:]
+
+test_df_time = df5mean_time[four_fifth_len:]
+
+
+#%%
+
+#X = train_df[['month', 'hour_minute', 'pm1.0', 'pm2.5', 'pm10.0', 'temp', 'humidity', 'speed']]
+X = train_df[['month', 'day', 'weekday', 'hour_minute', 'pm1.0', 'pm2.5', 'pm10.0', 'temp', 'humidity', 'speed']]
+y = train_df[['pm2.5_next_hour']]
+
+
+#%%
+
+#test_X = test_df[['month', 'hour_minute', 'pm1.0', 'pm2.5', 'pm10.0', 'temp', 'humidity', 'speed']]
+test_X = test_df[['month', 'day', 'weekday', 'hour_minute', 'pm1.0', 'pm2.5', 'pm10.0', 'temp', 'humidity', 'speed']]
+test_y = test_df[['pm2.5_next_hour']]
+
+#%%
+# Fit the model
+model = linear_model.LinearRegression(normalize=True)
+model.fit(X, y)
+#%%
+# See the coefficients of our model
+a = model.coef_
+b = model.intercept_
+print(a)
+print(b)
+
+
+#%%
+
+
+for i in range(len(X.columns)):
+    print('Coefficient for %10s:\t%s' % (X.columns[i], model.coef_[0][i]))
+
+
+#%%
+# Calculate mean squared error for training set & test set
+predict_train_y = model.predict(X)
+predict_y = model.predict(test_X)
+
+train_mse = metrics.mean_squared_error(y, predict_train_y)
+test_mse = metrics.mean_squared_error(test_y, predict_y)
+
+print('Train MSE:\t %s' % train_mse)
+print('Test MSE:\t %s' % test_mse)
+
+
+#%%
+# Add explicitly converter
+pd.plotting.register_matplotlib_converters()
+# Plt
+plt.figure(figsize=(12, 7))
+plt.plot(test_df_time, test_df['pm2.5_next_hour'], label='actual values')
+plt.plot(test_df_time, predict_y, label='predict values')
+plt.legend()
+plt.show()
 
 
 
